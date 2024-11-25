af79175e7a5d09ba2982ea1e1549542e5e2e2ee9
hyeshin4 <kimhyeshin1234@gmail.com>
2024-11-25 20:44:06 +0900

    first_specialize_itemFunction

--- Sources/2048.cpp
+++ Sources/2048.cpp
@@ -10,25 +10,31 @@ using namespace std;
 #define DOWN 's'
 
 // 전역변수들 정의
-int score = 0;         // Score
-const int numCell = 4; // 보드판의 가로 세로 길이 (기본 4x4)
+int score = 0;                    // Score
+const int numCell = 4;            // 보드판의 가로 세로 길이 (기본 4x4)
 int board[numCell][numCell] = {}; // 게임판 초기화
 
 // ANSI 기반 화면 관리
-void clearScreen() {
+void clearScreen()
+{
     cout << "\033[2J\033[1;1H";
 }
 
 // 메뉴 표시 함수
-void displayMenu(const string modes[], int modeCount, int selected) {
+void displayMenu(const string modes[], int modeCount, int selected)
+{
     clearScreen();
     cout << "==========================" << endl;
     cout << "        게임 모드         " << endl;
     cout << "==========================" << endl;
-    for (int i = 0; i < modeCount; ++i) {
-        if (i == selected) {
+    for (int i = 0; i < modeCount; ++i)
+    {
+        if (i == selected)
+        {
             cout << "> \033[1;32m" << modes[i] << "\033[0m" << endl;
-        } else {
+        }
+        else
+        {
             cout << "  " << modes[i] << endl;
         }
     }
@@ -36,16 +42,18 @@ void displayMenu(const string modes[], int modeCount, int selected) {
     cout << "W: 위로, S: 아래로, 스페이스: 선택" << endl;
 }
 
-
 // 사용될 함수들
 void new_num(void); // 세부 기능 2 (랜덤 2의배수 숫자 생성)
 void new_num_or_item(void);
-void draw(void); // 세부 기능 1 (3x3 게임판 만들기)
+void draw(void);            // 세부 기능 1 (3x3 게임판 만들기)
 void check_game_over(void); // 세부 기능 6 (게임 승리)
+void handleItem(int &current, int &next, int i, int j, int dx, int dy, int act);
+
 
 // 세부 기능 4 (사용자 이동기능-입력받기)
 // 단일 키 입력 받기 (getch 대체)
-char getInput() {
+char getInput()
+{
     char buf = 0;
     struct termios oldAttr, newAttr;
     tcgetattr(STDIN_FILENO, &oldAttr);
@@ -58,7 +66,7 @@ char getInput() {
 }
 
 // 2048 게임의 메인 루프
-void run2048() 
+void run2048()
 {
     // 게임을 진행하는 데 필요한 변수 선언
     int key;     // 사용자 입력 변수
@@ -68,118 +76,137 @@ void run2048()
     srand(time(NULL)); // 난수생성 시드값
     new_num();         // 초기값 2개 생성
     new_num();
-    
+
     draw(); // 임의의 수 2개 생성 후 게임판 그리기
 
     // 게임 시작
     while (1)
     {
         key = getInput(); // 사용자 입력을 key에 저장
-        act = 0;       
+        act = 0;
 
         // 세부 기능 3 (사용자 입력에 따른 블록 합체 및 이동 기능)
         switch (key)
         {
         case LEFT:
-            for (i = 0; i < numCell; i++)
-            { // 각 행에 대해 처리
-                for (j = 1; j <= numCell-1; j++)
-                { // 두 번째 칸부터 마지막 칸까지
-                    for (r = j; r > 0; r--)
-                    { // 현재 칸을 왼쪽으로 이동
-                        if (board[i][r] == 0 || board[i][r] > 10000)
-                            break; // case1.비어있거나 이미 병합된 경우
-                        if (board[i][r - 1] != 0 && board[i][r - 1] != board[i][r])
-                            break; // case2.병합 불가능한 경우 (연속 같은 수)
-                        if (board[i][r - 1] == 0)
-                            board[i][r - 1] = board[i][r]; // case3.왼쪽이 비어있으면 이동
-                        else if (board[i][r] == board[i][r - 1])
-                        { // case4.같은 숫자가 충돌하면 병합
-                            board[i][r - 1] *= 2;       // 2배로 병합
-                            board[i][r - 1] += 10000;   // 병합된 숫자 표시
-                            score += 2 * (board[i][r]); // 점수 업데이트
+            for (int i = 0; i < numCell; i++)
+            {
+                for (int j = 1; j < numCell; j++)
+                { // 두 번째 칸부터 시작
+                    if (board[i][j] != 0)
+                    {
+                        int k = j;
+                        while (k > 0 && board[i][k - 1] == 0)
+                        { // 빈 칸으로 이동
+                            board[i][k - 1] = board[i][k];
+                            board[i][k] = 0;
+                            k--;
+                            act++;
                         }
-                        // case3,4(움직임 발생)인 경우만 해당
-                        board[i][r] = 0; // 현재 칸 비우기
-                        act++;           // 이동/병합 발생 표시
+                        if (k > 0)
+                        { // 아이템 및 합체 처리
+                            handleItem(board[i][k], board[i][k - 1], i, k, 0, -1, act);
+                            if (board[i][k - 1] == board[i][k])
+                            { // 합체
+                                board[i][k - 1] *= 2;
+                                score += board[i][k - 1];
+                                board[i][k] = 0;
+                                act++;
+                            }
+                        }
                     }
                 }
             }
             break;
 
         case RIGHT:
-            for (i = 0; i < numCell; i++)
-            { // 각 행에 대해 처리
-                for (j = numCell-2; j >= 0; j--)
-                { // 마지막 -1 칸부터 첫 번째 칸까지
-                    for (r = j; r < numCell-1; r++)
-                    { // 현재 칸을 오른쪽으로 이동
-                        if (board[i][r] == 0 || board[i][r] > 10000)
-                            break;
-                        if (board[i][r + 1] != 0 && board[i][r + 1] != board[i][r])
-                            break;
-                        if (board[i][r + 1] == 0)
-                            board[i][r + 1] = board[i][r];
-                        else if (board[i][r] == board[i][r + 1])
-                        {
-                            board[i][r + 1] *= 2;
-                            board[i][r + 1] += 10000;
-                            score += 2 * (board[i][r]);
+            for (int i = 0; i < numCell; i++)
+            {
+                for (int j = numCell - 2; j >= 0; j--)
+                { // 오른쪽에서 두 번째 칸부터 시작
+                    if (board[i][j] != 0)
+                    {
+                        int k = j;
+                        while (k < numCell - 1 && board[i][k + 1] == 0)
+                        { // 빈 칸으로 이동
+                            board[i][k + 1] = board[i][k];
+                            board[i][k] = 0;
+                            k++;
+                            act++;
                         }
-                        board[i][r] = 0;
-                        act++;
+                        if (k < numCell - 1)
+                        { // 아이템 및 합체 처리
+                            handleItem(board[i][k], board[i][k + 1], i, k, 0, 1, act);
+                            if (board[i][k + 1] == board[i][k])
+                            { // 합체
+                                board[i][k + 1] *= 2;
+                                score += board[i][k + 1];
+                                board[i][k] = 0;
+                                act++;
+                            }
+                        }
                     }
                 }
             }
             break;
 
         case UP:
-            for (j = 0; j < numCell; j++)
-            { // 각 열에 대해 처리
-                for (i = 1; i <= numCell-1; i++)
-                { // 두 번째 행부터 마지막 행까지
-                    for (r = i; r > 0; r--)
-                    { // 현재 칸을 위로 이동
-                        if (board[r][j] == 0 || board[r][j] > 10000)
-                            break;
-                        if (board[r - 1][j] != 0 && board[r - 1][j] != board[r][j])
-                            break;
-                        if (board[r - 1][j] == 0)
-                            board[r - 1][j] = board[r][j];
-                        else if (board[r][j] == board[r - 1][j])
-                        {
-                            board[r - 1][j] *= 2;
-                            board[r - 1][j] += 10000;
-                            score += 2 * (board[r][j]);
+            for (int j = 0; j < numCell; j++)
+            {
+                for (int i = 1; i < numCell; i++)
+                { // 두 번째 행부터 시작
+                    if (board[i][j] != 0)
+                    {
+                        int k = i;
+                        while (k > 0 && board[k - 1][j] == 0)
+                        { // 빈 칸으로 이동
+                            board[k - 1][j] = board[k][j];
+                            board[k][j] = 0;
+                            k--;
+                            act++;
                         }
-                        board[r][j] = 0;
-                        act++;
+                        if (k > 0)
+                        { // 아이템 및 합체 처리
+                            handleItem(board[k][j], board[k - 1][j], k, j, -1, 0, act);
+                            if (board[k - 1][j] == board[k][j])
+                            { // 합체
+                                board[k - 1][j] *= 2;
+                                score += board[k - 1][j];
+                                board[k][j] = 0;
+                                act++;
+                            }
+                        }
                     }
                 }
             }
             break;
 
         case DOWN:
-            for (j = 0; j < numCell; j++)
-            { // 각 열에 대해 처리
-                for (i = numCell-2; i >= 0; i--)
-                { // 마지막-1 행부터 첫 번째 행까지
-                    for (r = i; r < numCell-1; r++)
-                    { // 현재 칸을 아래로 이동
-                        if (board[r][j] == 0 || board[r][j] > 10000)
-                            break;
-                        if (board[r + 1][j] != 0 && board[r + 1][j] != board[r][j])
-                            break;
-                        if (board[r + 1][j] == 0)
-                            board[r + 1][j] = board[r][j];
-                        else if (board[r][j] == board[r + 1][j])
-                        {
-                            board[r + 1][j] *= 2;
-                            board[r + 1][j] += 10000;
-                            score += 2 * (board[r][j]);
+            for (int j = 0; j < numCell; j++)
+            {
+                for (int i = numCell - 2; i >= 0; i--)
+                { // 아래에서 두 번째 행부터 시작
+                    if (board[i][j] != 0)
+                    {
+                        int k = i;
+                        while (k < numCell - 1 && board[k + 1][j] == 0)
+                        { // 빈 칸으로 이동
+                            board[k + 1][j] = board[k][j];
+                            board[k][j] = 0;
+                            k++;
+                            act++;
                         }
-                        board[r][j] = 0;
-                        act++;
+                        if (k < numCell - 1)
+                        { // 아이템 및 합체 처리
+                            handleItem(board[k][j], board[k + 1][j], k, j, 1, 0, act);
+                            if (board[k + 1][j] == board[k][j])
+                            { // 합체
+                                board[k + 1][j] *= 2;
+                                score += board[k + 1][j];
+                                board[k][j] = 0;
+                                act++;
+                            }
+                        }
                     }
                 }
             }
@@ -205,34 +232,45 @@ void run2048()
 }
 
 // 메인 함수
-int main() {
+int main()
+{
     const int modeCount = 4;
 
-    string modes[modeCount] = { "모드 1: 2048 게임", "모드 2: 아이템 모드", "모드 3: 목표달성 모드", "모드 4: 설명서" };
-    
+    string modes[modeCount] = {"모드 1: 2048 게임", "모드 2: 아이템 모드", "모드 3: 목표달성 모드", "모드 4: 설명서"};
+
     int selected = 0; // 선택된 모드
 
-    while (true) {
+    while (true)
+    {
         displayMenu(modes, modeCount, selected);
         char input = getInput();
 
-        if (input == 'w' || input == 'W') {
+        if (input == 'w' || input == 'W')
+        {
             selected = (selected - 1 + modeCount) % modeCount;
-        } else if (input == 's' || input == 'S') {
+        }
+        else if (input == 's' || input == 'S')
+        {
             selected = (selected + 1) % modeCount;
-        } else if (input == ' ') {
-            if (selected == 0) {
+        }
+        else if (input == ' ')
+        {
+            if (selected == 0)
+            {
                 clearScreen();
                 cout << "모드 1: 2048 게임을 시작합니다!" << endl;
                 run2048(); // 2048 게임 실행
-            } 
-            else if (selected == 3) {
+            }
+            else if (selected == 3)
+            {
                 clearScreen();
                 cout << "1. 목표" << endl;
-                cout << "-같은 숫자를 합쳐 더 높은 숫자를 생성하며, 최종적으로 2048을 만들면 승리합니다." << endl << endl;
+                cout << "-같은 숫자를 합쳐 더 높은 숫자를 생성하며, 최종적으로 2048을 만들면 승리합니다." << endl
+                     << endl;
                 cout << "2. 기본 규칙" << endl;
                 cout << "-한 번의 움직임마다 보드에 새로운 숫자 2 또는 4가 랜덤 위치에 나타납니다." << endl;
-                cout << "-같은 숫자가 충돌하면 합쳐져 두 배가 됩니다 (예: 2 + 2 = 4, 4 + 4 = 8)." << endl << endl;
+                cout << "-같은 숫자가 충돌하면 합쳐져 두 배가 됩니다 (예: 2 + 2 = 4, 4 + 4 = 8)." << endl
+                     << endl;
                 cout << "3. 게임 방법" << endl;
                 cout << "-WSAD로 위/아래/왼쪽/오른쪽으로 모든 숫자를 이동시킬 수 있습니다." << endl;
                 cout << "-숫자는 선택한 방향으로 이동하며, 이동 가능한 가장 먼 칸까지 이동합니다." << endl;
@@ -240,8 +278,9 @@ int main() {
                 // 다음 시간에 뒤로가기 또는 시간 딜레이 기능 만들기
                 continue;
             }
-            
-            else {
+
+            else
+            {
                 clearScreen();
                 cout << modes[selected] << "은(는) 아직 구현되지 않았습니다!" << endl;
                 break;
@@ -253,12 +292,45 @@ int main() {
 }
 
 // 함수 구체화
+void handleItem(int &current, int &next, int i, int j, int dx, int dy, int act)
+{
+    if (next == -1)
+    { // 점프 아이템
+        int nx = i + 2 * dx;
+        int ny = j + 2 * dy;
+        if (nx >= 0 && nx < numCell && ny >= 0 && ny < numCell && board[nx][ny] == current)
+        {
+            board[nx][ny] *= 2;
+            score += 2 * current;
+            current = 0;
+            next = 0; // 점프 아이템 소멸
+            act++;
+        }
+    }
+    else if (next == -2)
+    { // 폭발 아이템
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                int nx = i + x, ny = j + y;
+                if (nx >= 0 && nx < numCell && ny >= 0 && ny < numCell)
+                {
+                    board[nx][ny] = 0; // 인접 블록 제거
+                }
+            }
+        }
+        current = 0;
+        next = 0; // 폭발 아이템 소멸
+        act++;
+    }
+}
 
 // 세부 기능 2 (랜덤 2의배수 숫자 생성)
 void new_num(void)
 {
     int i, j, cnt = 0;
-    int *p0[numCell*numCell] = {0}; // 포인터 배열 p0를 선언
+    int *p0[numCell * numCell] = {0}; // 포인터 배열 p0를 선언
 
     for (i = 0; i < numCell; i++)
     {
@@ -277,29 +349,39 @@ void new_num(void)
     // rand() % 100 < 80 조건을 사용해 80% 확률로 2를, 20% 확률로 4를 생성하여 선택된 빈 칸에 대입
 }
 
-void new_num_or_item() {
+void new_num_or_item()
+{
     int i, j, cnt = 0;
     int *p0[numCell * numCell] = {0};
 
-    for (i = 0; i < numCell; i++) {
-        for (j = 0; j < numCell; j++) {
-            if (board[i][j] == 0) {
+    for (i = 0; i < numCell; i++)
+    {
+        for (j = 0; j < numCell; j++)
+        {
+            if (board[i][j] == 0)
+            {
                 p0[cnt] = &board[i][j];
                 cnt++;
             }
         }
     }
 
-    if (cnt == 0) return; // 빈 칸이 없으면 추가하지 않음
+    if (cnt == 0)
+        return; // 빈 칸이 없으면 추가하지 않음
 
     int randIndex = rand() % cnt;
     int randChance = rand() % 100;
 
-    if (randChance < 10) {
+    if (randChance < 10)
+    {
         *p0[randIndex] = -1; // 10% 확률로 점프 아이템 (J)
-    } else if (randChance < 15) {
+    }
+    else if (randChance < 15)
+    {
         *p0[randIndex] = -2; // 5% 확률로 폭발 블럭 (B)
-    } else {
+    }
+    else
+    {
         *p0[randIndex] = (rand() % 100 < 80) ? 2 : 4; // 숫자 추가
     }
 }
@@ -316,18 +398,29 @@ void draw(void)
             cout << "----|";
         }
         cout << "----" << endl;
-        for (int j = 0; j < numCell; j++) {
-            if (board[i][j] == -1) {
+        for (int j = 0; j < numCell; j++)
+        {
+            if (board[i][j] == -1)
+            {
                 cout << " J  |";
-            } else if (board[i][j] == -2) {
+            }
+            else if (board[i][j] == -2)
+            {
                 cout << " B  |";
-            } else {
+            }
+            else
+            {
                 cout << board[i][j];
-                if (j == numCell - 1) break;
-                if (board[i][j] < 10) cout << "   |";
-                else if (board[i][j] < 100) cout << "  |";
-                else if (board[i][j] < 1000) cout << " |";
-                else if (board[i][j] < 10000) cout << "|";
+                if (j == numCell - 1)
+                    break;
+                if (board[i][j] < 10)
+                    cout << "   |";
+                else if (board[i][j] < 100)
+                    cout << "  |";
+                else if (board[i][j] < 1000)
+                    cout << " |";
+                else if (board[i][j] < 10000)
+                    cout << "|";
             }
         }
         cout << endl;
@@ -337,7 +430,7 @@ void draw(void)
         cout << "----|";
     }
     cout << "----" << endl;
-    printf("\nScore : %d \n", score); 
+    printf("\nScore : %d \n", score);
 }
 
 // 세부 기능 6 (게임 승리)
@@ -347,34 +440,34 @@ void check_game_over(void)
 
     for (i = 0; i < numCell; i++)
         for (j = 0; j < numCell; j++)
-            if (board[i][j] > 10000){ // 10000을 초과하는 경우
+            if (board[i][j] > 10000)
+            { // 10000을 초과하는 경우
                 cout << "10000점을 돌파했습니다. 게임을 종료합니다.";
-                exit(0); }
+                exit(0);
+            }
 
     for (i = 0; i < numCell; i++)
         for (j = 0; j < numCell; j++)
             if (board[i][j] == 0) // 빈 칸이 있는 경우 게임진행
-                return; 
-
-    for (i = 0; i < numCell-1; i++)
-        for (j = 0; j < numCell-1; j++)
-            if (board[i][j] == board[i + 1][j] || board[i][j] == board[i][j + 1]) 
-            // 합칠 수 있는 수가 있는 경우 재개
                 return;
 
-// 만약 위의 for문에 범위를 4미만으로 할 경우 board[4][j]라는 존재하지 않는 정보를 비교하게 됨
-// 때문에 3행 비교와 3열 비교는 각 각 분리해야 함
-    for (i = 0; i < numCell-1; i++)
-        if (board[i][numCell-1] == board[i + 1][numCell-1]) //가장 오른쪽 열을 검사
+    for (i = 0; i < numCell - 1; i++)
+        for (j = 0; j < numCell - 1; j++)
+            if (board[i][j] == board[i + 1][j] || board[i][j] == board[i][j + 1])
+                // 합칠 수 있는 수가 있는 경우 재개
+                return;
+
+    // 만약 위의 for문에 범위를 4미만으로 할 경우 board[4][j]라는 존재하지 않는 정보를 비교하게 됨
+    // 때문에 3행 비교와 3열 비교는 각 각 분리해야 함
+    for (i = 0; i < numCell - 1; i++)
+        if (board[i][numCell - 1] == board[i + 1][numCell - 1]) // 가장 오른쪽 열을 검사
             return;
 
-    for (j = 0; j < numCell-1; j++)
-        if (board[numCell-1][j] == board[numCell-1][j + 1]) //가장 아래쪽 행을 검사
+    for (j = 0; j < numCell - 1; j++)
+        if (board[numCell - 1][j] == board[numCell - 1][j + 1]) // 가장 아래쪽 행을 검사
             return;
 
-
-
-// 위 4가지 경우를 모두 충족시키지 못한 경우
+    // 위 4가지 경우를 모두 충족시키지 못한 경우
     cout << "Game Over..";
     exit(0);
 }
